1022cd8c6f9f4c7e2fe6b1fe7d8aeafbc18c5ef0
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2023-07-05 09:09:41 +0700

    read data

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,69 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import { json, type V2_MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Customers" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
+export const loader = async () => {
+  return json({
+    customers: await db.customer.findMany({ orderBy: { name: "asc" } }),
+  });
+};
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
-      <h1 className="uppercase">Welcome to Remix, Prisma ORM and Tailwind</h1>
+      <section className="antialiased bg-gray-100 text-gray-600 h-screen px-4">
+        <div className="flex flex-col justify-center h-full">
+          <div className="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
+            <header className="px-5 py-4 border-b border-gray-100">
+              <h2 className="font-semibold text-gray-800">Customers</h2>
+            </header>
+            <div className="p-3">
+              <div className="overflow-x-auto">
+                <table className="table-auto w-full">
+                  <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
+                    <tr>
+                      <th className="p-2 whitespace-nowrap">
+                        <div className="font-semibold text-left">Name</div>
+                      </th>
+                      <th className="p-2 whitespace-nowrap">
+                        <div className="font-semibold text-left">Email</div>
+                      </th>
+                      <th className="p-2 whitespace-nowrap">
+                        <div className="font-semibold text-left">Phone</div>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="text-sm divide-y divide-gray-100">
+                    {data?.customers.map((customer) => (
+                      <tr key={customer.id}>
+                        <td className="p-2 whitespace-nowrap">
+                          <div className="text-left">{customer.name}</div>
+                        </td>
+                        <td className="p-2 whitespace-nowrap">
+                          <div className="text-left">{customer.email}</div>
+                        </td>
+                        <td className="p-2 whitespace-nowrap">
+                          <div className="text-left font-medium text-green-500">
+                            {customer.phone}
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
